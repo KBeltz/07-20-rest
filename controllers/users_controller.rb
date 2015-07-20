517854883d925ef311a2df269342765e46aca455
Kristin Beltz <beltz08@me.com>
2015-07-20 13:38:26 -0500

    Trying to delete stuff.

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -1,21 +1,27 @@
-get "/users/new" do
+get "/users/new" do # Bring up form to create User
   erb :"users/new"
 end
 
-post "/users/create" do
+post "/users/create" do # Create a User
   email = params["email"]
   password = BCrypt::Password.create(params["password"])
   user = User.create({email: email, password: password})
   redirect "/users/#{user.id}"
 end
 
-get "/users/:id" do
-  @user = User.find(params[:id])
-  erb :"users/show_user"
+get "/users" do # Show all Users
+  @array_of_users = User.all
+  erb :"/users/all_users"
 end
 
+post "/users/delete_form" do # Delete a specific User
+  user = User.find(params[:id])
+  @user_email = @user.email
+  user.destroy
+  erb :"users/deleted"
+end
 
-
-get "/users" do # Show all users
-  @array_of_users = User.all
+get "/users/:id" do # Find a specific User
+  @user = User.find(params[:id])
+  erb :"users/show_user"
 end
