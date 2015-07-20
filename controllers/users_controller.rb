0eb54d755b392f71ab9887b08c3e927d5f43d2dc
Kristin Beltz <beltz08@me.com>
2015-07-20 12:41:14 -0500

    User create

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -3,5 +3,19 @@ get "/users/new" do
 end
 
 post "/users/create" do
-  
+  email = params["email"]
+  password = BCrypt::Password.create(params["password"])
+  User.create({email: email, password: password})
+  redirect "/users/#{user.id}" 
+end
+
+get "/users/:id" do
+  @user = User.find(:id)
+  erb :"users/show_user"
+end
+
+
+
+get "/users" do # Show all users
+  @array_of_users = User.all?
 end
