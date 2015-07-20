7f918dd5740f6a85f6e247e5c2189f7304e1cb40
Kristin Beltz <beltz08@me.com>
2015-07-20 12:26:42 -0500

    New user form

--- models/user.rb
+++ models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   validates :email, presence: true
 
-  unless ActiveRecord::Base.connection.table_exists(:users)
+  unless ActiveRecord::Base.connection.table_exists?(:users)
     ActiveRecord::Base.connection.create_table :users do |t|
       t.text :email
       t.text :password
+    end
+  end
 end
