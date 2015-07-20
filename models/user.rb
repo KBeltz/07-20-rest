ac7e6da5e9dc1e01c9bf978007b18b48e620a270
Kristin Beltz <beltz08@me.com>
2015-07-20 12:20:08 -0500

    create table stuff

--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   validates :email, presence: true
+
+  unless ActiveRecord::Base.connection.table_exists(:users)
+    ActiveRecord::Base.connection.create_table :users do |t|
+      t.text :email
+      t.text :password
 end
