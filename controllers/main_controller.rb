c76b95d7a5008314acf6e8ac1b26e2bc79f6556a
Kristin Beltz <beltz08@me.com>
2015-07-21 14:34:08 -0500

    start adding authentication:

--- controllers/main_controller.rb
+++ controllers/main_controller.rb
@@ -1,3 +1,15 @@
 get "/" do
   erb :"/main/home"
 end
+#change to post
+post "/login" do
+  # auth stuff
+  @user = User.where({email: params["email"]})
+  actual_password = BCrypt::Password.new(@user[0].password)
+  attempted_password = params["password"]
+    if actual_password == attempted_password
+      redirect "/users/@user[0].id"
+    else
+      redirect "/"
+    end
+end
