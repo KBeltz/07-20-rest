ec2e01d6f7c422d3749763bde2bccc783bdb0ee2
Kristin Beltz <beltz08@me.com>
2015-07-21 12:35:36 -0500

    Fix edit routes

--- controllers/stories_controller.rb
+++ controllers/stories_controller.rb
@@ -10,25 +10,24 @@ post "/users/:user_id/stories" do # Create a Story
   redirect "/users/#{user.id}/stories/#{story.id}"
 end
 
-###########################################################################
-# working ^^
-###########################################################################
-# get "/stories/delete_form" do
-#   @array_of_stories = Story.all
-#   erb :"/stories/delete_form"
-# end
-
 get "/users/:user_id/stories/:id/edit" do # Story edit form
   @story = Story.find(params[:id])
   erb :"/stories/edit_form"
 end
 
-put "/users/:user_id/stories/:id" do #Update a specific Stories
-  story = Story.find(params[:id])
+###########################################################################
+# working ^^
+###########################################################################
+
+put "/users/:user_id/stories/:id" do #Update a specific Story
+  @user = User.find(params[:user_id])
+  @story = Story.find(params[:id])
   story.update_attributes(title: params["title"])
-  story.save
+  @story.save
   erb :"/stories/success"
 end
+
+###########################################################################
 
 post "/users/:user_id/stories/:id" do # Delete a specific Story
   story = Story.find(params["story_id"])
@@ -36,13 +35,9 @@ post "/users/:user_id/stories/:id" do # Delete a specific Story
   erb :"/stories/deleted"
 end
 
-###########################################################################
 get "/users/:user_id/stories/:id" do # Find a specific Story
   @story = Story.find(params[:id])
   @user = User.find(@story.user_id)
   @user_stories = @user.stories
   erb :"/stories/show_story"
 end
-
-#/users/:user_id/stories
-#/users/:user_id/stories/new
